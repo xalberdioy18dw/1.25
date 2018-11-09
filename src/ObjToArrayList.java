a2a7460a7ffec4b425444c212e5c2ad3de3c0ba2
ik013043z1 <ik013043z1@H013043.NET>
2018-11-09 11:36:26 +0100

    2.0

--- src/ObjToArrayList.java
+++ src/ObjToArrayList.java
@@ -2,5 +2,17 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class ObjToArrayList {
-
+	public static void main (String[]args) {
+		try {
+			File file = new File(args[0]);
+			Scanner sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+	            String i = sc.nextLine();
+	            System.out.println(i);
+			}
+			 catch (FileNotFoundException e) {
+					System.out.println("File is not founded.");
+			 }
+		}
+	}
 }
