6aee4cec63a902087ac19d6db38e98eaf040ba9b
xalberdioy18dw <43572704+xalberdioy18dw@users.noreply.github.com>
2018-11-11 21:49:38 +0100

    3.0

--- src/ObjToArrayList.java
+++ src/ObjToArrayList.java
@@ -1,18 +1,27 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class ObjToArrayList {
 	public static void main (String[]args) {
+		ArrayList<String> footballmatches = new Arraykust<String>();
 		try {
 			File file = new File(args[0]);
 			Scanner sc = new Scanner(file);
 			while (sc.hasNextLine()) {
 	            String i = sc.nextLine();
 	            System.out.println(i);
+	            FootballMatch match = new FootballMatch();
+	            for (int z = 0; z < word.length; z++) {
+	            	match.getLocalTeam
+	            }
 			}
-			 catch (FileNotFoundException e) {
-					System.out.println("File is not founded.");
-			 }
+			System.out.println(footballmatches);
+			System.out.println("There are " + footballmatches.size() + " matches." );
 		}
+		 catch (FileNotFoundException e) {
+			System.out.println("File is not founded.");
+			 }
+		
 	}
 }
