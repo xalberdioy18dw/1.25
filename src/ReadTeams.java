e2db6fad2aaf949802bcf20d4ffb6fc100c39355
ik013043z1 <ik013043z1@H013043.NET>
2018-11-23 11:37:44 +0100

    8.1

--- src/ReadTeams.java
+++ src/ReadTeams.java
@@ -18,22 +18,9 @@ public class ReadTeams {
 				team.setcountry(fteams[2]);
 				team.setleague(fteams[3]);
 				teamsinfo.add(team);
-				
-			File file2 = new File(args[1]);
-			Scanner sc2 = new Scanner(file);
-			while (sc.hasNextLine()) {
-	            String matches = sc.nextLine();
-	            System.out.println(matches);	            
-	            String[] fmatch = matches.split("::");
-	            Match match = new Match();	         
-	            match.setLocalTeam(fmatch[0]);
-				match.setVisitorTeam(fmatch[1]);
-				match.setGoalsLocal(Integer.parseInt(fmatch[2]));
-				match.setGoalsVisitor(Integer.parseInt(fmatch[3]));
-				footballmatches.add(match);
-			}
-			}
+			}	
 		}
+
 		catch (FileNotFoundException e) {
 				System.out.println("File is not founded.");
 		}
